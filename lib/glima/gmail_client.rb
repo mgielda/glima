38d03cd5d637377762deba16db2f1512a6f2236a
Yoshinari Nomura <nom@quickhack.net>
2017-04-20 20:46:34 +0900

    Hide datastore under GmailClient

--- lib/glima/gmail_client.rb
+++ lib/glima/gmail_client.rb
@@ -71,13 +71,50 @@ module Glima
     :batch
 
 
+    # Fine nearby messages from pivot_message
+    # `Nearby' message:
+    #   + has same From: address
+    #   + has near Date: field (+-1day)
+    # with the pivot_message.
+    def nearby_messages(pivot_mail)
+      from  = "from:#{pivot_mail.from}"
+      date1 = (pivot_mail.date.to_date - 1).strftime("after:%Y/%m/%d")
+      date2 = (pivot_mail.date.to_date + 1).strftime("before:%Y/%m/%d")
+      query = "#{from} #{date1} #{date2}"
+      scan_batch("+all", query) do |message|
+        yield message
+      end
+    end
+
+    # * message types by format:
+    #   | field/fromat:   | list | minimal | raw | value type      |
+    #   |-----------------+------+---------+-----+-----------------|
+    #   | id              | ○   | ○      | ○  | string          |
+    #   | threadId        | ○   | ○      | ○  | string          |
+    #   | labelIds        |      | ○      | ○  | string[]        |
+    #   | snippet         |      | ○      | ○  | string          |
+    #   | historyId       |      | ○      | ○  | unsinged long   |
+    #   | internalDate    |      | ○      | ○  | long            |
+    #   | sizeEstimate    |      | ○      | ○  | int             |
+    #   |-----------------+------+---------+-----+-----------------|
+    #   | payload         |      |         |     | object          |
+    #   | payload.headers |      |         |     | key/value pairs |
+    #   | raw             |      |         | ○  | bytes           |
+    #
+    def get_user_smart_message(id)
+      fmt = if @datastore.exist?(id) then "minimal" else "raw" end
+      @client.get_user_message('me', id, format: fmt) do |m, err|
+        yield(if m then @datastore.update(m) else nil end, err)
+      end
+    end
+
     def online?
       Socket.getifaddrs.select {|i|
         i.addr.ipv4? and ! i.addr.ipv4_loopback?
       }.map(&:addr).map(&:ip_address).length > 0
     end
 
-    def initialize(config)
+    def initialize(config, datastore)
       authorizer = Authorizer.new(config.client_id,
                                   config.client_secret,
                                   Google::Apis::GmailV1::AUTH_SCOPE,
@@ -85,7 +122,7 @@ module Glima
 
       credentials = authorizer.credentials(config.default_user) ||
                     authorizer.auth_interactively(config.default_user)
-
+      @datastore = datastore
       @client = Google::Apis::GmailV1::GmailService.new
       @client.client_options.application_name = 'glima'
       @client.authorization = credentials
@@ -98,5 +135,34 @@ module Glima
       @imap ||= Glima::ImapWatch.new("imap.gmail.com", @client.authorization)
       @imap.wait(label)
     end
+
+    def batch_on_messages(ids, &block)
+      batch do |batch_client|
+        ids.each do |id|
+          fmt = if @datastore.exist?(id) then "minimal" else "raw" end
+
+          batch_client.get_user_message('me', id, format: fmt) do |m, err|
+            fail "#{err}" if err
+            message = @datastore.update(m)
+            yield message
+          end
+        end
+      end
+    end
+
+    def scan_batch(folder, search_or_range = nil, &block)
+      qp = Glima::QueryParameter.new(folder, search_or_range)
+      list_user_messages('me', qp.to_hash) do |res, error|
+        fail "#{error}" if error
+        ids = res.messages.map(&:id)
+        batch_on_messages(ids) do |message|
+          yield message if block
+        end
+        # context.save_page_token(res.next_page_token)
+      end
+    rescue Glima::QueryParameter::FormatError => e
+      STDERR.print "Error: " + e.message + "\n"
+    end
+
   end # class GmailClient
 end # module Glima
