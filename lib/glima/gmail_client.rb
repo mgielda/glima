27eed989b033e106cf29f3d1ef75bc233f0a74a5
Yoshinari Nomura <nom@quickhack.net>
2017-04-25 16:59:27 +0900

    Return mail object from get_user_smart_message

--- lib/glima/gmail_client.rb
+++ lib/glima/gmail_client.rb
@@ -103,9 +103,13 @@ module Glima
     #
     def get_user_smart_message(id)
       fmt = if @datastore.exist?(id) then "minimal" else "raw" end
+
+      mail = nil
       @client.get_user_message('me', id, format: fmt) do |m, err|
-        yield(if m then Glima::Resource::Mail.new(@datastore.update(m)) else nil end, err)
+        mail = Glima::Resource::Mail.new(@datastore.update(m)) if m
+        yield(mail, err)
       end
+      return mail
     end
 
     def online?
