e8e2bb1168c9287e56b334cdb8ca39e72037235f
Yoshinari Nomura <nom@quickhack.net>
2017-05-18 13:27:00 +0900

    Add scan format

--- lib/glima/command/scan.rb
+++ lib/glima/command/scan.rb
@@ -6,13 +6,25 @@ module Glima
 
         index = 1
         client.scan_batch(folder, search_or_range) do |mail|
-          if format == :mew
+          case format
+          when :mew
             puts mail.format_mew(index)
+          when :archive
+            puts format_archive_friendly(mail)
           else
             puts mail.format_summary(index)
           end
           index += 1
         end
+      end
+
+      private
+
+      def format_archive_friendly(mail)
+        date = mail.date.strftime("%Y-%m-%d-%H%M%S")
+        # Replace unsafe chars for filename
+        subject = mail.subject.tr('!/:*?"<>|\\', '！／：＊？″＜＞｜＼').gsub(/[\s　]/, '')
+        return "#{mail.id} #{date}-#{mail.id}-#{subject}.eml"
       end
 
     end # class Scan
