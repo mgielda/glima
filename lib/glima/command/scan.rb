c2949039b41327a3e9d05812b76fd78c5a11f2e1
Michael Gielda <mgielda@antmicro.com>
2020-09-09 01:23:10 +0200

    Add a "legible" email option

--- lib/glima/command/scan.rb
+++ lib/glima/command/scan.rb
@@ -11,6 +11,8 @@ module Glima
             puts mail.format_mew(index)
           when :archive
             puts format_archive_friendly(mail)
+          when :legible
+            puts format_legible(mail)
           else
             puts mail.format_summary(index)
           end
@@ -25,6 +27,14 @@ module Glima
         # Replace unsafe chars for filename
         subject = mail.subject.tr('!/:*?"<>|\\', '！／：＊？″＜＞｜＼').gsub(/[\s　]/, '')
         return "#{mail.id} #{date}-#{mail.id}-#{subject}.eml"
+      end
+
+      def format_legible(mail)
+        date = mail.date.strftime("%Y-%m-%d-%H%M%S")
+        # Remove delim char
+        subject = mail.subject.tr('|', '')
+
+        return "#{mail.id}|#{date}|#{mail.subject}|#{mail.header['from']}|#{mail.header['to']}"
       end
 
     end # class Scan
